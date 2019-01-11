94481a4b687860d117baec5f17e7ef216d0764f3
ESON <ESON@DESKTOP-0JDBN5R>
2019-01-11 10:43:15 +0800

    hello.my god

--- src/main/java/book/fromnnot.java
+++ src/main/java/book/fromnnot.java
@@ -5,6 +5,7 @@ public class fromnnot {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am from mynotebook");
+		System.out.println("very intering.");
 	}
 
 }
